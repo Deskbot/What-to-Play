b31cf9bb98ee51ea4ac9f505b82da76259da3983
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-27 23:33:54 +0000

    Add regexes for metacritic platform names

--- src/metacritic.ts
+++ src/metacritic.ts
@@ -9,6 +9,31 @@ export interface MetacriticResult {
     userscore: number | undefined;
 }
 
+export const platforms: ReadonlyArray<readonly [RegExp, string]> = [
+    [/^(ps|playstation).*5$/i, "playstation-5"],
+    [/^(ps|playstation).*4$/i, "playstation-4"],
+    [/^(ps|playstation).*3$/i, "playstation-3"],
+    [/^(ps|playstation).*2$/i, "playstation-2"],
+    [/^(ps|playstation).*1?$/i, "playstation"],
+    [/^(ps|playstation).*v(ita)?$/i, "playstation-vita"],
+    [/^(ps|playstation).*(p|portable)$/i, "psp"],
+    [/^xbox.*s?e?(ries)?.*$/i, "xbox-series-x"],
+    [/^xbox.*(one|1)$/i, "xbox-one"],
+    [/^xbox.*360$/i, "xbox-360"],
+    [/^xbox$/i, "xbox"],
+    [/^pc|windows|mac|linux|desktop$/i, "pc"],
+    [/^(nintendo.*)?switch$/i, "switch"],
+    [/^(nintendo.*)?wii.*u$/i, "wii-u"],
+    [/^(nintendo.*)?wii$/i, "wii"],
+    [/^(nintendo.*)?gamecube$/i, "gamecube"],
+    [/^n(intendo)?.*64$/i, "nintendo-64"],
+    [/^(nintendo.*)?3ds$/i, "3ds"],
+    [/^(nintendo.*)?ds.*$/i, "ds"],
+    [/^(nintendo.*)?(game.*boy.*advance|gba)$/i, "game-boy-advance"],
+    [/^ios|android|mobile|(smart)?phone|tablet$/i, "ios"],
+    [/^(sega.*)?dreamcast$/i, "dreamcast"],
+];
+
 export async function getInfo(game: string, preferredPlatforms: string[]): Promise<MetacriticResult | undefined> {
     const searchUrl = `https://www.metacritic.com/search/game/${game}/results`;
 
