251aad0427d881ba06bb3c2551bb89764a2073c4
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-27 22:58:03 +0000

    Output sensible CSV

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,28 @@ import * as fs from "fs";
 import * as minimist from "minimist";
 import * as process from "process";
 import * as readline from "readline";
+import { metacritic } from "./api";
 import * as steam from "./steam";
 import { csvFriendly, printable } from "./util";
+
+export interface ResultJson {
+    game: string;
+    metacritic: metacritic.MetacriticResult | undefined;
+    steam: steam.SteamResult | undefined;
+}
+
+export type ResultCSV = Record<
+    "Game"
+    | "Steam Name"
+    | "Steam URL"
+    | "Steam All Time % Positive"
+    | "Steam Recent % Positive"
+    | "Metacritic Name"
+    | "Metacritic URL"
+    | "Metacritic Critic Score"
+    | "Metacritic User Score" ,
+    string
+>;
 
 interface Printer {
     (game: string): void;
@@ -45,8 +65,12 @@ function main() {
     }
 }
 
-export function getData(game: string): Promise<steam.SteamResult | undefined> {
-    return steam.getInfo(game);
+export async function getData(game: string): Promise<ResultJson> {
+    return {
+        game,
+        metacritic: await metacritic.getInfo(game, []),
+        steam: await steam.getInfo(game),
+    };
 }
 
 export async function getJson(game: string): Promise<string> {
@@ -57,15 +81,27 @@ export async function getJson(game: string): Promise<string> {
 export async function getCsv(game: string): Promise<string> {
     const buffer = [] as string[];
 
-    const data = await getData(game)
+    const data = await getData(game);
 
     if (data === undefined) return "";
 
+    const newData = {
+        "Game": data.game,
+        "Steam Name": data.steam?.name ?? "",
+        "Steam URL": data.steam?.url ?? "",
+        "Steam All Time % Positive": data.steam?.allTimeScore ?? "",
+        "Steam Recent % Positive": data.steam?.recentScore ?? "",
+        "Metacritic Name": data.metacritic?.name ?? "",
+        "Metacritic URL": data.metacritic?.url ?? "",
+        "Metacritic Critic Score": data.metacritic?.metascore ?? "",
+        "Metacritic User Score": data.metacritic?.userscore ?? "",
+    };
+
     // iterate through in the same order every time guaranteed
-    const keys = Object.keys(data) as (keyof steam.SteamResult)[];
+    const keys = Object.keys(newData) as (keyof ResultCSV)[];
 
     for (const key of keys) {
-        buffer.push(csvFriendly(printable(data[key])));
+        buffer.push(csvFriendly(printable(newData[key])));
         buffer.push(",");
     }
 
