04ad9ce56436efde4e6133b7d77fd81af7ae209e
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-28 14:29:06 +0000

    Add HLTB name

--- src/how-long-to-beat.ts
+++ src/how-long-to-beat.ts
@@ -3,6 +3,7 @@ import fetch from "node-fetch";
 import { bug, nonNaN } from "./util";
 
 export interface HowLongToBeatResult {
+    name: string;
     mainStory: number;
     mainPlusExtra: number;
     completionist: number;
@@ -19,18 +20,30 @@ export async function getData(game: string): Promise<HowLongToBeatResult | undef
     .then(res => res.text());
 
     const searchPage = cheerio.load(html);
+    const searchResult = searchPage(".search_list_details").first();
 
-    const timeElems = searchPage(".search_list_details_block")
+    if (searchResult.length === 0) return undefined;
+
+    const name = searchResult
+        .find("a")
+        .first()
+        .text();
+
+    if (!name) bug();
+
+    const timeElems = searchResult
+        .find(".search_list_details_block")
         .first()
         .find(".time_100");
 
-    if (timeElems.length === 0) return undefined;
+    if (timeElems.length === 0) bug();
 
     const mainStory = getTimeFromBlock(timeElems.get(0));
     const mainPlusExtra = getTimeFromBlock(timeElems.get(1));
     const completionist = getTimeFromBlock(timeElems.get(2));
 
     return {
+        name,
         mainStory,
         mainPlusExtra,
         completionist,
