1a4018501b9743962454d1cc1db584ad8845a52f
Thomas Richards <Deskbot@users.noreply.github.com>
2020-12-30 18:58:37 +0000

    Add test framework

---
+++ src/util.test.ts
@@ -0,0 +1,21 @@
+import { ok, strictEqual } from "assert";
+import * as util from "./util";
+import { Test, TestSuite } from "testyts";
+
+@TestSuite()
+export class UtilTestSuite {
+    @Test()
+    average() {
+        ok(Number.isNaN(util.average([])));
+        strictEqual(util.average([1]), 1);
+        strictEqual(util.average([1,3]), 2);
+    }
+
+    @Test()
+    escapeDoubleQuotes() {
+        strictEqual(util.escapeDoubleQuotes('',    '""'), '');
+        strictEqual(util.escapeDoubleQuotes('str', '""'), 'str');
+        strictEqual(util.escapeDoubleQuotes('"',   '""'), '""');
+        strictEqual(util.escapeDoubleQuotes('""',  '""'), '""""');
+    }
+}
