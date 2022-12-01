5235331b1917f460026e32ef9bdb256df6031eed
Melody <melody@melody.blue>
2022-12-01 11:20:38 +1000

    Quick and dirty howlongtobeat package integration

--- src/howlongtobeat.ts
+++ src/howlongtobeat.ts
@@ -1,8 +1,7 @@
-import * as cheerio from "cheerio";
-import fetch from "node-fetch";
-import * as querystring from "querystring";
 import { closestSearchResult } from "./search";
-import { bug } from "./util";
+import { HowLongToBeatService, HowLongToBeatEntry } from 'howlongtobeat';
+
+let hltbService = new HowLongToBeatService();
 
 export interface HowLongToBeatResult {
     name: string;
@@ -17,104 +16,29 @@ export interface HowLongToBeatResult {
     url: string;
 }
 
-/**
- * A map from the name of each measure of completion time used by howlongtobeat.com
- * to the key used in the result of this program.
- */
-const fieldMap: Record<string, keyof HowLongToBeatResult["times"] | undefined> = {
-    "Main Story": "mainStory",
-    "Main + Extra": "mainPlusExtra",
-    "Completionist": "completionist",
-    "Solo": "solo",
-    "Co-Op": "coop",
-    "Vs.": "vs",
-};
-
 export async function getData(game: string): Promise<HowLongToBeatResult | undefined> {
-    const searchPage = await getSearchPage(game);
-
-    // choose the best result
-
-    const searchResultElems = searchPage(".search_list_details");
-    const searchResults = searchResultElems.toArray().map(searchPage);
+    const results = await hltbService.search(game);
+    console.error(`Searching for ${game}`)
+    console.error(results)
 
     const bestResult = closestSearchResult(
         game,
-        searchResults,
-        searchResult => searchResult.find("a").first().text().trim()
-    );
+        results,
+        searchResult => searchResult.name
+    )
 
-    if (!bestResult) return undefined;
-
-    // get data about the game out of the best result
-
-    // get name
-    const nameElem = bestResult.find("a").first();
-    const name = nameElem.text();
-    if (!name) bug();
-
-    // get url
-    const url = "https://howlongtobeat.com/" + nameElem.attr("href");
-
-    // get completion times
-
-    const gridElems = bestResult
-        .find(".search_list_tidbit,.search_list_tidbit_short,.search_list_tidbit_long")
-        .toArray();
-
-    const times = {} as HowLongToBeatResult["times"];
-
-    let i = 0;
-    while (true) {
-        // the elements alternate between labels and fields
-        const fieldElem = gridElems[i];
-        const timeElem = gridElems[i + 1];
-        i += 2;
-
-        if (fieldElem === undefined) break;
-
-        const fieldName = searchPage(fieldElem).text().trim();
-        const time = getTimeFromElem(searchPage(timeElem));
-
-        // transform the given field name into the key we want to use in the result
-        const key = fieldMap[fieldName];
-        if (key === undefined) bug();
-
-        // add the time to the output
-        times[key] = time;
+    
+    if (bestResult == undefined) return undefined
+    
+    const correctType: HowLongToBeatResult = {
+        name: bestResult?.name,
+        times: {
+            mainStory: bestResult?.gameplayMain,
+            mainPlusExtra: bestResult?.gameplayMainExtra,
+            completionist: bestResult?.gameplayCompletionist,
+        },
+        url: `https://howlongtobeat.com/game/${bestResult.id}`
     }
 
-    return {
-        name,
-        times,
-        url,
-    };
-}
-
-/** search for game on the website */
-async function getSearchPage(game: string): Promise<cheerio.Root> {
-    const searchUrl = "https://howlongtobeat.com/search_results?page=1";
-    const gameStr = querystring.escape(game);
-    const postData = `queryString=${gameStr}&t=games&sorthead=popular&sortd=0&plat=&length_type=main&length_min=&length_max=&v=&f=&g=&detail=&randomize=0`;
-
-    const res = await fetch(searchUrl, {
-        method: "POST",
-        body: postData,
-        headers: {
-            "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Origin": "https://howlongtobeat.com",
-            "Referer": "https://howlongtobeat.com/",
-        }
-    });
-
-    return cheerio.load(await res.text());
-}
-
-function getTimeFromElem(elem: cheerio.Cheerio): number | undefined {
-    const timeStr = elem.html()?.replace("½", ".5");
-    const time = parseFloat(timeStr as any); // undefined returns NaN
-
-    if (Number.isNaN(time)) return undefined; // likely lack of data i.e. when timeStr === "--"
-    return time;
+    return correctType
 }
